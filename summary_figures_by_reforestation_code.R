1fc9094c527fd5554a78ea5f155d37592cef5578
kellyjsmith <93109030+kellyjsmith@users.noreply.github.com>
2024-04-13 10:37:38 -0700

    update summaries

--- summary_figures_by_reforestation_code.R
+++ summary_figures_by_reforestation_code.R
@@ -1,7 +1,10 @@
+assigned_activities = readRDS("assigned_activities_2022.RDS")
 
+assigned_activities_filtered = assigned_activities %>%
+  filter(diff_years >= 0)
 
-# Modify the net_activities and gross_activities calculations to include PRODUCTIVI and year
-net_activities_productivity <- map2_dfr(
+# Modify the net_activities and gross_activities calculations to include REFORESTAT and year
+net_activities_reforestation <- map2_dfr(
   comb_fire_diff$fire_year,
   comb_fire_diff$diff_years,
   function(x,y,assigned_activities){
@@ -11,21 +14,21 @@ net_activities_productivity <- map2_dfr(
     if(dim(filtered)[1]==0){
       return(NULL)
     }else{
-      result <- filtered |> group_by(VB_ID,ACTIVITY_TYPE,PRODUCTIVI) |> 
+      result <- filtered |> group_by(Event_ID,ACTIVITY_TYPE,REFORESTAT) |> 
         summarize(geometry=st_union(geometry),n_dissolved=n(),
                   Ig_Year=first(Ig_Year),diff_years=first(diff_years),
-                  PRODUCTIVI=first(PRODUCTIVI))
+                  REFORESTAT=first(REFORESTAT))
       result$ref_year <-result$Ig_Year+result$diff_years
       result$net_area <- st_area(result)
       
-      # Summarize net area by PRODUCTIVI in separate columns
-      # result <- result |> pivot_wider(names_from = PRODUCTIVI, values_from = net_area, values_fill = 0)
+      # Summarize net area by REFORESTAT in separate columns
+      # result <- result |> pivot_wider(names_from = REFORESTAT, values_from = net_area, values_fill = 0)
       
       return(result)
     }
   },assigned_activities=assigned_activities)
 
-gross_activities_productivity <- map2_dfr(
+gross_activities_reforestation <- map2_dfr(
   comb_fire_diff$fire_year,
   comb_fire_diff$diff_years,
   function(x,y,assigned_activities){
@@ -34,7 +37,7 @@ gross_activities_productivity <- map2_dfr(
     if(dim(filtered)[1]==0){
       return(NULL)
     }else{
-      result <-filtered |> group_by(VB_ID,ACTIVITY_TYPE,PRODUCTIVI)|> 
+      result <-filtered |> group_by(Event_ID,ACTIVITY_TYPE,REFORESTAT)|> 
         summarize(gross_area=sum(st_area(geometry)),
                   Ig_Year=first(Ig_Year),diff_years=first(diff_years))
       result$ref_year <-result$Ig_Year+result$diff_years
@@ -43,26 +46,26 @@ gross_activities_productivity <- map2_dfr(
   },assigned_activities=assigned_activities)
 
 
-#### Summarize Treatment Area by Productivity Class ####
+#### Summarize Treatment Area by reforestation Class ####
 
 # Convert sf object to data frame
-gross_activities_prod_df = st_drop_geometry(gross_activities_productivity)
-net_activities_prod_df = st_drop_geometry(net_activities_productivity)
+gross_activities_prod_df = st_drop_geometry(gross_activities_reforestation)
+net_activities_prod_df = st_drop_geometry(net_activities_reforestation)
 
 # Group by ref_year and summarize acres
-gross_planting_by_productivity = gross_activities_prod_df %>%
-  filter(ACTIVITY_TYPE == "planting") %>%
-  group_by(ref_year, PRODUCTIVI) %>%
-  summarize(gross_planting_acres = as.numeric(sum(gross_area)/4046.86), .groups = 'drop')
-net_planting_by_productivity = net_activities_prod_df %>%
-  filter(ACTIVITY_TYPE == "planting") %>%
-  group_by(ref_year, PRODUCTIVI) %>%
-  summarize(net_planting_acres = as.numeric(sum(net_area)/4046.86), .groups = 'drop')
+gross_REFORESTAT = gross_activities_prod_df %>%
+  # filter(ACTIVITY_TYPE == "planting") %>%
+  group_by(ref_year, REFORESTAT) %>%
+  summarize(gross_acres = as.numeric(sum(gross_area)/4046.86), .groups = 'drop')
+net_REFORESTAT = net_activities_prod_df %>%
+  # filter(ACTIVITY_TYPE == "planting") %>%
+  group_by(ref_year, REFORESTAT) %>%
+  summarize(net_acres = as.numeric(sum(net_area)/4046.86), .groups = 'drop')
 
 
 # Filter the data frames for the specified columns
-net_by_prod_ref_year_filter1 = net_planting_by_productivity
-gross_by_prod_ref_year_filter1 = gross_planting_by_productivity
+net_by_REFORESTAT_filter1 = net_REFORESTAT
+gross_by_REFORESTAT_filter1 = gross_REFORESTAT
 
 # net_by_prod_ref_year_filter1 = net_by_prod_ref_year[, c("ref_year", "planting", "release", "replant", "thin")]
 # gross_by_prod_ref_year_filter1 = gross_by_prod_ref_year[, c("ref_year", "planting", "release", "replant", "thin")]
@@ -75,22 +78,22 @@ gross_by_prod_ref_year_filter1 = gross_planting_by_productivity
 # Add a new column to identify the type of acres and rename acreage columns
 # net_by_prod_ref_year_filter1_long$type = "net_planting_acres"
 # gross_by_prod_ref_year_filter1_long$type = "gross_planting_acres"
-net_by_prod_ref_year_filter1$type = "net_planting_acres"
-gross_by_prod_ref_year_filter1$type = "gross_planting_acres"
+net_by_REFORESTAT_filter1$type = "net_acres"
+gross_by_REFORESTAT_filter1$type = "gross_acres"
 
-colnames(net_by_prod_ref_year_filter1)[colnames(net_by_prod_ref_year_filter1)=="net_planting_acres"] <- "value"
-colnames(gross_by_prod_ref_year_filter1)[colnames(gross_by_prod_ref_year_filter1)=="gross_planting_acres"] <- "value"
+colnames(net_by_REFORESTAT_filter1)[colnames(net_by_REFORESTAT_filter1)=="net_acres"] <- "value"
+colnames(gross_by_REFORESTAT_filter1)[colnames(gross_by_REFORESTAT_filter1)=="gross_acres"] <- "value"
 
 # Combine the data frames
 # combined_prod_ref_year_filter1 = rbind(net_by_prod_ref_year_filter1_long, gross_by_prod_ref_year_filter1_long)
-combined_prod_ref_year_filter1 = rbind(net_by_prod_ref_year_filter1, gross_by_prod_ref_year_filter1)
+combined_by_REFORESTAT_filter1 = rbind(net_by_REFORESTAT_filter1, gross_by_REFORESTAT_filter1)
 
 # Spread the data frame to have gross_acres and net_acres in separate columns
-combined_prod_ref_year_filter1 = spread(combined_prod_ref_year_filter1, key = type, value = value)
+combined_by_REFORESTAT_filter1 = spread(combined_by_REFORESTAT_filter1, key = type, value = value)
 
 # Add a new column for the difference between gross and net acres
-combined_prod_ref_year_filter1$difference = combined_prod_ref_year_filter1$gross_planting_acres - 
-  combined_prod_ref_year_filter1$net_planting_acres
+combined_by_REFORESTAT_filter1$difference = combined_by_REFORESTAT_filter1$gross_acres - 
+  combined_by_REFORESTAT_filter1$net_acres
 
 # Plot difference
 ggplot(combined_prod_ref_year_filter1, aes(x = ref_year)) +
@@ -102,21 +105,21 @@ ggplot(combined_prod_ref_year_filter1, aes(x = ref_year)) +
   theme(legend.position="bottom", legend.box = "horizontal")
 
 # Plot gross and net with facets
-ggplot(combined_prod_ref_year_filter1, aes(x = ref_year)) +
-  geom_line(aes(y = gross_planting_acres, color = "Gross Planting Acres")) +
-  geom_line(aes(y = net_planting_acres, color = "Net Planting Acres")) +
-  ggtitle("Gross and Net Acres Planted by Productivity Class in R5") +
+ggplot(combined_by_REFORESTAT_filter1, aes(x = ref_year)) +
+  geom_line(aes(y = gross_acres, color = "Gross Acres")) +
+  geom_line(aes(y = net_acres, color = "Net Acres")) +
+  ggtitle("Gross and Net Acres Treated by Reforestation Class in R5") +
   labs(x = "Activity Year", y = "Treatment Acres", color = "Type of Acres") +
   scale_x_continuous(breaks = seq(1992, 2023, 4)) +
-  facet_wrap(~ PRODUCTIVI, ncol = 2) +
+  facet_wrap(~ REFORESTAT, ncol = 2) +
   theme_bw()+
   theme(legend.position="bottom", legend.box = "horizontal")
 
 # Plot gross and net with facets
 ggplot(combined_prod_ref_year_filter1) +
-  ggtitle("Net Postfire Acres Planted in R5 by Productivity Class, 1993 - 2022") +
-  geom_bar(aes(x = PRODUCTIVI, y = net_planting_acres), stat = "identity") +
-  labs(x = "Productivity Class", y = "Net Planted Acres") +
+  ggtitle("Net Postfire Acres Planted in R5 by Reforestation Class, 1993 - 2022") +
+  geom_bar(aes(x = REFORESTAT, y = net_planting_acres), stat = "identity") +
+  labs(x = "reforestation Class", y = "Net Planted Acres") +
   theme_bw()+
   theme(legend.position="bottom", legend.box = "horizontal")
 
