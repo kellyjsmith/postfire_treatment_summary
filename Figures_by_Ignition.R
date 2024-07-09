7a0e008359e545647533de86f2c83be7e2ea8d0b
kellyjsmith <93109030+kellyjsmith@users.noreply.github.com>
2024-07-09 13:56:09 -0700

    create new labels

--- Figures_by_Ignition.R
+++ Figures_by_Ignition.R
@@ -1,4 +1,7 @@
+#
 
+library(tidyverse)
+library(sf)
 
 setwd("C:/Users/smithke3/OneDrive - Oregon State University/Kelly/Output")
 
@@ -40,6 +43,19 @@ dev.off()
 # Combined, total cumulative area and within 5 years, by ignition year
 gross_net_cumulative = readRDS("gross_net_cumulative.RDS")
 gross_net_cumulative_5years = readRDS("gross_net_cumulative_5years.RDS")
+new_labels = data.frame(ACTIVITY_TYPE = c("Certified_Planted","Fuels","Plant","Stand_Exam",
+"Survey","TSI","Harvest_NonSalv","Prescription","Replant","SitePrep_NonChem","Thin","Harvest_Salvage",
+"Need_by_Fire","SitePrep_Chem","Prune","Need_by_Failure","Certified_TSI","Mapping"),
+ACTIVITY_TYPE_LABELS = c("Planting Certification","Fuels","Planting","Stand Exam",
+                    "Survey","TSI","Harvest (Non-Salvage)","Prescription","Replanting/Fill-in","Site Prep (Non-Chemical)","Thin","Salvage",
+                    "Reforestation Need by Fire","Site Prep (Chemical)","Prune","Reforestation Need by Failure","TSI Certification","Mapping"))
+gross_net_cumulative = merge(gross_net_cumulative, new_labels, by = "ACTIVITY_TYPE", all.x = TRUE)
+gross_net_cumulative_5years = merge(gross_net_cumulative_5years, new_labels, by = "ACTIVITY_TYPE", all.x = TRUE)
+jpeg(filename = "gross_net_cumulative_5years_allyears.jpg",
+     width = 900, height = 1200,
+     quality = 75,
+     bg = "white",
+     symbolfamily="default")
 ggplot() +
   theme_bw() +
   geom_area(data = gross_net_cumulative, aes(x = end, y = gross_area_ac, fill = "Gross Acres to Date"),alpha = 0.9) +
@@ -50,10 +66,10 @@ ggplot() +
   ggtitle("R5 Reforestation Activities Completed to Date, by Ignition Year") +
   labs(x = "Ignition Year", y = "Activity Acres") +
   scale_x_continuous(breaks = seq(1990, 2020, 6)) +
-  facet_wrap(~ ACTIVITY_TYPE, ncol = 3, scales = "free_y") +
-  scale_fill_manual(values = c("Gross Acres to Date" = "gray","Net Acres to Date" = "lightgray")) +
-  scale_color_manual(values = c("Gross Acres within 5 Years" = "blue", "Net Acres within 5 Years" = "red", "5-yr Gross = 5-yr Net (~1 m^2)" = "green")) +
-  theme(legend.position="bottom", legend.box = "horizontal", plot.title = element_text(size=12)) +
+  facet_wrap(~ ACTIVITY_TYPE_LABELS, ncol = 3, scales = "free_y") +
+  scale_fill_manual(values = c("Gross Acres to Date" = "gray","Net Acres to Date" = "lightgray")) +scale_color_manual(values = c("Gross Acres within 5 Years" = "blue", "Net Acres within 5 Years" = "red", "5-yr Gross = 5-yr Net (~1 m^2)" = "green")) +
+  theme(legend.position="bottom", legend.box = "horizontal", plot.title = element_text(size=22), text = element_text(size=18)) +
   guides(fill=guide_legend(title=NULL, nrow=2), color=guide_legend(title=NULL,nrow=2))
 # theme(legend.position = "none") +
 # guides(fill="none")
+dev.off()
